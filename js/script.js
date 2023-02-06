e0ae7a566e38f6629517a12228956d632c8f2dee
Anton <cecon_76@mail.ru>
2023-02-06 18:03:44 +0500

    added cycles

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 "use strict";
 
+
+
+
+
+
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
+
+
 const personalMovieDB = {
+    
+
     count: numberOfFilms,
     movies: {
 
@@ -15,10 +24,33 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastOfFilms = prompt('Один из просмотренных фильмов?', ''),
-      lastRateOfFilms = prompt('На сколько вы его оцените?', '');
+if(personalMovieDB.count<10){
+    alert('Просмотрено довольно мало фильмов')
+}else if(personalMovieDB.count<=30){
+    alert('Вы классический зритель')
+}else if(personalMovieDB.count>30){
+    alert('Вы киноман')
+}else{
+    alert('Произошла ошибка')
+};
 
-personalMovieDB.movies[lastOfFilms] = lastRateOfFilms;
+for(let i=0; i<2; i++){
+    const lastOfFilms = prompt('Один из просмотренных фильмов?', ''),
+          lastRateOfFilms = prompt('На сколько вы его оцените?', ''); 
+    if(lastOfFilms != null && lastRateOfFilms != null && lastOfFilms!= '' && lastRateOfFilms != '' && lastOfFilms.length < 50){
+        console.log('done');
+    }else{
+        console.log('error');
+        i--;
+    }
+    personalMovieDB.movies[lastOfFilms] = lastRateOfFilms;
+    
+}
+
+
+
+
+
 console.log(personalMovieDB); 
 
 
